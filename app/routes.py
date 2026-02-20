ef83421678cccb5f1a243fde06a9d2f164e03306
steviejaydee <74107570+steviejaydee@users.noreply.github.com>
2026-02-20 12:33:42 +0000

    Implement user database and login functionality
    Added user database initialisation and login validation.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from app import app
 from datetime import timedelta
+import json
+import os
+
 
 app.permanent_session_lifetime = timedelta(days = 365)
+def init_db():
+    #creates json for user database if it does not yet exist
+    if not os.path.exists('users.json'):
+        dummy_users = [
+            {"first_name": "Alice", "email": "axb3759@bham.ac.uk", "password": "password123"},
+            {"first_name": "Bob", "email": "bxs7290@bham.ac.uk", "password": "securepass"},
+            {"first_name": "Charlie", "email": "cxp1296@bham.ac.uk", "password": "bham2026"}
+        ]
+        with open('users.json', 'w') as f:
+            json.dump(dummy_users, f, indent  = 4)
 
 @app.route('/')
 def index():
+    init_db()
     #Check user is logged in
     if 'email' not in session:
         return redirect(url_for('login'))
@@ -21,12 +35,18 @@ def login():
         email = request.form.get('email')
         password = request.form.get('password')
 
-        #Making sure email ends with the correct domain for UoB (it also makes sure everything is filled in but the form requires this anyways, may not be essential?):
-        if first_name and email and email.endswith('@student.bham.ac.uk') and password:
-            #Establish a permanent session (persists between session) so user is logged out automatically after a year:
+        #Check user database to see if user email and password already exist.
+        user = None
+        with open('users.json', 'r') as f:
+            users_db = json.load(f)
+            for u in users_db:
+                if u['email'] == email and  u['password'] == password:
+                    user = u
+                    break
+        if user:
             session.permanent = True
-            session['email'] = email
-            session ['first_name'] = first_name
+            session['email'] = user['email']
+            session['first_name'] = user['first_name']
             return redirect(url_for('index'))
     else:
         flash('Invalid sign in, please try again (maybe check your email is correct).')
