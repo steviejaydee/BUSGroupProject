e229647a0e01bf93690ce538df39548d9382bd50
charlottedinsdale <charlottedins01@gmail.com>
2026-04-17 16:38:43 +0100

    remove file duplicates from outside app, style emergency page

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
-
+from flask_mail import Mail
 app = Flask(__name__, static_folder='static')
 app.secret_key = "secret"
 
+app.config['MAIL_SERVER'] = '127.0.0.1'
+app.config['MAIL_PORT'] = 8025
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = False
+
+mail =  Mail(app)
 from app import routes
