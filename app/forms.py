e596fa01a15ecc2e6cdbd4822fd767fd87fc764e
steviejaydee <74107570+steviejaydee@users.noreply.github.com>
2026-04-24 12:21:01 +0100

    Fix formatting of dob field in TriageForm

--- app/forms.py
+++ app/forms.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired, Length
 class TriageForm(FlaskForm):
   name = StringField("What is your full name?", 
                      validators=[DataRequired()])
-  dob = DateField("What is your date of birth?", 
-                  format="%Y-%m-%d", 
+  dob = DateField("What is your date of birth?",
+                  format="%Y-%m-%d",
                   validators=[DataRequired()])
   problem = TextAreaField("Give a brief description of the problems you are experiencing",  
                           validators=[DataRequired(), Length(0, 500)])
